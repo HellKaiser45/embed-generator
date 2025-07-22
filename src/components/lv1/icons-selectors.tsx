46ad6a7a3946949d4325e08e08493ba4e1b5ea56
HellKaiser45 <julien.delage0@gmail.com>
2025-07-22 20:35:32 +0200

    refactor: replace button with label for checkbox integration

--- src/components/lv1/icons-selectors.tsx
+++ src/components/lv1/icons-selectors.tsx
@@ -1,28 +1,28 @@
-import { component$, useSignal, useComputed$, $ } from '@builder.io/qwik';
+import { component$ } from '@builder.io/qwik';
 import { Icon } from '../basics/icons';
 import { iconRegistry } from '../icons-registry/icons.data';
 import { type IconName } from '../icons-registry/icons.types';
 import BetterButton from '~/components/basics/button';
 
-
 export const IconSelector = component$(() => {
-
 
   return (
     <ul class="list bg-base-100 rounded-box shadow-md">
       {Object.entries(iconRegistry).map(([name, _]) => (
-        <li class="list-row" key={name} >
-
-          <BetterButton>
-            <input type="checkbox" checked={false} class="checkbox checkbox-primary" />
-            <Icon name={name as IconName} />
-            <input />
-
-          </BetterButton>
+        <li class="list-row" key={name}>
+          <label class="relative flex items-center cursor-pointer">
+            <input
+              type="checkbox"
+              checked={false}
+              class="checkbox checkbox-primary peer absolute top-0 left-0 w-full h-full opacity-0"
+            />
+            <BetterButton class="peer-checked:bg-secondary">
+              <span class="text-primary min-w-24 justify-start flex">{name} :</span>
+              <Icon name={name as IconName} />
+            </BetterButton>
+          </label>
         </li>
-      ))
-      }
-
+      ))}
     </ul>
   );
 });
